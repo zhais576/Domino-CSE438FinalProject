d3efc7289004c74b262a7b01b72b353056154c8d
Jack Zhai <zhais@wustl.edu>
2021-07-10 16:57:58 -0700

    Initial Commit

---
+++ Domino-CSE438FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Domino-CSE438FinalProject
+//
+//  Created by Zhai on 7/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
