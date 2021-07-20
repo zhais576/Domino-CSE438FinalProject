07073cde6bd1bbe26ee7248b8c1ab498c5b6fd4f
Jack Zhai <zhais@wustl.edu>
2021-07-20 09:30:35 -0700

    deselect tile works

--- Domino-CSE438FinalProject/Tile.swift
+++ Domino-CSE438FinalProject/Tile.swift
@@ -11,20 +11,55 @@ class Tile : UIView {
     
     var sides: [Int] = [-1,-1]
     var faceImage: UIImage?
+    var originalCenter: CGPoint! //copies the original center after init
     
     init(int1: Int, int2: Int, image: UIImage, frame: CGRect) {
+        
         self.sides = [int1, int2]
         self.faceImage = image
+        self.originalCenter = CGPoint(x: frame.origin.x + frame.size.width / 2, y: frame.origin.y + frame.size.height / 2)
         super.init(frame: frame)
-        
+
+        //handles image of the tile
         let myImage = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
         myImage.image = faceImage
         myImage.layer.zPosition = 1
         self.addSubview(myImage)
-        print("finished new tile")
+        
+        //create and add gesture to the tile
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.dragging(gesture:)))
+        self.addGestureRecognizer(gesture)
+        self.isUserInteractionEnabled = true
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    //this handles the dragging gesture
+    //tile must be added as a subview
+    @objc func dragging(gesture: UIPanGestureRecognizer){
+        let translation = gesture.translation(in: self.superview!)
+        let tile35 = gesture.view!
+        tile35.center = CGPoint(x: tile35.center.x + translation.x, y: tile35.center.y + translation.y)
+        gesture.setTranslation(CGPoint.zero, in: self.superview!)
+        //when dragging is released
+        if gesture.state == UIGestureRecognizer.State.ended{
+            //snap back to place
+            tile35.center = originalCenter
+            //decide the tile is played to the left or right
+            playedLeftOrRight(gesture: gesture)
+        }
+    }
+   
+    //checks the gesture's location and decide left or right in superview
+    func playedLeftOrRight(gesture: UIPanGestureRecognizer){
+        if(gesture.location(in: self.superview).x < 195){
+            print("Played to the left")
+        }else{
+            print("Played to the right")
+        }
+    }
+    
+    
 }
--- Domino-CSE438FinalProject/ViewController.swift
+++ Domino-CSE438FinalProject/ViewController.swift
@@ -11,27 +11,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theGameBoard: GameBoard!
     
-    let tile35 = Tile(int1: 3, int2: 5, image: UIImage(named: "35")!,frame: CGRect(x: 200, y: 300, width: 50, height: 100))
+    let tile35 = Tile(int1: 3, int2: 5, image: UIImage(named: "35")!,frame: CGRect(x: 182, y: 611, width: 50, height: 100))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        _ = GameController()
+        
+        _ = GameController(team1Name: "t1", team2Name: "t2", player1Name: "p1", player2Name: "p2", player3Name: "p3", player4Name: "p4")
+        
         view.addSubview(tile35)
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.dragging(gesture:)))
-        tile35.addGestureRecognizer(gesture)
-        tile35.isUserInteractionEnabled = true
     }
 
-    @objc func dragging(gesture: UIPanGestureRecognizer){
-        let originalCenter = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-        let translation = gesture.translation(in: self.view)
-        let tile35 = gesture.view!
-        tile35.center = CGPoint(x: tile35.center.x + translation.x, y: tile35.center.y + translation.y)
-        gesture.setTranslation(CGPoint.zero, in: self.view)
-        if gesture.state == UIGestureRecognizer.State.ended{
-            tile35.center = originalCenter
-        }
-    }
+    
     
 
 }
