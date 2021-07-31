a734ba631f78e5f2f5edae64b93fc93d2e2601d6
Lucas Santana <lucassantanapr@gmail.com>
2021-07-31 18:40:46 -0400

    New Loading Screen :)

--- Domino-CSE438FinalProject/ViewController.swift
+++ Domino-CSE438FinalProject/ViewController.swift
@@ -21,21 +21,64 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        p1TextField.autocorrectionType = .no
-        p2TextField.autocorrectionType = .no
-        p3TextField.autocorrectionType = .no
-        p4TextField.autocorrectionType = .no
+        setupView()
         navigationController?.setNavigationBarHidden(true, animated: false)//hides navigation bar
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         //startButton.isHidden = true
+        setupView()
         clearAll() //clears all textfield and user defaults
     }
     
     //MARK: - helper functions
     
-    @IBAction func readyPressed(_ sender: Any) { //checks if all names are entered
+    func setupView() {
+        let bckgrnd = UIColor(red: 0.071, green: 0.106, blue: 0.212, alpha: 1.0)
+        let teal = UIColor(red: 0.345, green: 0.773, blue: 0.910, alpha: 1)
+        let pink = UIColor(red: 0.827, green: 0.204, blue: 0.459, alpha: 1)
+        
+        
+        
+        let textfields = [p1TextField, p2TextField, p3TextField, p4TextField]
+        for tf in textfields {
+            tf?.backgroundColor = bckgrnd
+            tf?.autocorrectionType = .no
+            tf?.textAlignment = .center
+            
+        // setup custom border citation: https://stackoverflow.com/questions/39939557/how-to-customize-uitextfield
+            tf?.borderStyle = .none
+            let border = CALayer()
+            border.frame = CGRect(x: 0, y: 0, width: (tf?.frame.width)!, height: (tf?.frame.height)!)
+            
+            border.borderWidth = 2.0
+            border.shadowRadius = 0.1088 * border.frame.width
+            border.shadowOpacity = 1
+            
+            
+            if tf == p1TextField || tf == p3TextField { // is in team 1
+                tf?.textColor = teal
+                border.borderColor = teal.cgColor
+                border.shadowColor = teal.cgColor
+            } else {                                    // is in team 2
+                tf?.textColor = pink
+                border.borderColor = pink.cgColor
+                border.shadowColor = pink.cgColor
+                
+            }
+            tf?.layer.addSublayer(border)
+            
+        }
+    }
+    
+    
+    
+    @IBAction func clearPressed(_ sender: Any) {
+        clearAll()
+    }
+    
+    @IBAction func startPressed(_ sender: Any) {
         if p1TextField.text == "" || p2TextField.text == "" || p3TextField.text == "" || p4TextField.text == ""{
             let namePrompt = UIAlertController(title: "Please enter all names", message: nil, preferredStyle: .alert)
             namePrompt.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
@@ -47,14 +90,6 @@ class ViewController: UIViewController {
         //reset scores in userdefaults
         UserDefaultsHandler().encode(data: 0, whereTo: .team1Score)
         UserDefaultsHandler().encode(data: 0, whereTo: .team2Score)
-        startButton.isHidden = false //display start button
-    }
-    
-    @IBAction func clearPressed(_ sender: Any) {
-        clearAll()
-    }
-    
-    @IBAction func startPressed(_ sender: Any) {
         let newGame = GamePlayViewController()
         navigationController?.pushViewController(newGame, animated: false)
     }
